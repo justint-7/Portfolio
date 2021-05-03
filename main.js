820129040a3a17ee1d3b994b79f99a1e07974fdf
justint-7 <justintseng97@gmail.com>
2021-05-03 18:33:50 -0400

    added projects

--- main.js
+++ main.js
@@ -13,23 +13,41 @@ portfolioApp.InitialSlideAnimation = () =>{
 portfolioApp.setProjectListeners = () =>{
     let x=0;
     let xPercent=0;
+    let xBar = 0;
+
     let isDragImages = false;
     let isDragBar=false;
+
     const projectSection = document.querySelector('.projects');
     const projectDisplay = document.querySelector('.projectDisplay');
     const currentBar = document.querySelector('.currentBar');
 
+    const maxDisplayLength= projectDisplay.scrollWidth;
+    const maxLeftPosition=  -maxDisplayLength + document.documentElement.clientWidth *.9;
+    const numOfProjects= projectDisplay.childElementCount;
+
+    
+
+
+
+
+
+
     projectDisplay.addEventListener('mousedown', (event)=>{
         event.preventDefault;
-        isDragImages =true;        
+        isDragImages =true;  
+   
     });
+
+    
+
 
     projectSection.addEventListener('mousemove', (event)=>{
         if (isDragImages){
-           if (x<=0 || x >= -3000 +document.documentElement.clientWidth *.9){
+           if (x<=0 || x >= maxLeftPosition){
                 x= x+event.movementX;
                 projectDisplay.style.left=`${x}px`;
-                xPercent= (x/(-3000 +document.documentElement.clientWidth *.9))*90;
+                xPercent= (x/(maxLeftPosition+ ((numOfProjects+2)*5)))*90;
                 if (xPercent>0 && xPercent<90){
                 currentBar.style.left=`${xPercent}%`;
                 } else if (xPercent<0){
@@ -41,23 +59,37 @@ portfolioApp.setProjectListeners = () =>{
            }
         }
         if (isDragBar){
-            if (xPercent>0){
-                
+            if (xPercent>=0 && xPercent <=90){
+                xPercent = xPercent + (event.movementX / (currentBar.clientWidth*10)*100);
+                currentBar.style.left=`${xPercent}%`;
+                x= (-3000+document.documentElement.clientWidth) * (xPercent/100);
+                console.log (x);
+                projectDisplay.style.left=`${x}px`;            
+
             }
         }
-        
     });
     projectSection.addEventListener('mouseup', (event)=>{
         isDragImages=false;
         isDragBar=false;
-        if (x>0){
+        if (x>0 || xPercent<0){
             x=0;
             projectDisplay.style.left=`${x}px`;
-            currentBar.style.left='0%'
-        } else if (x<-3000 +document.documentElement.clientWidth *.9){
+            xPercent=0;
+            currentBar.style.left=`${xPercent}%`;
+            projectDisplay.classList.add('transitionOOB');
+            setTimeout(() => {
+                projectDisplay.classList.remove('transitionOOB');
+            }, 500);
+        } else if (x<-3000 +document.documentElement.clientWidth *.9 || xPercent >90){
             x=-3000 +document.documentElement.clientWidth *.9;
             projectDisplay.style.left=`${x}px`;
-            currentBar.style.left='90%'
+            xPercent=90;
+            currentBar.style.left=`${xPercent}%`;
+            projectDisplay.classList.add('transitionOOB');
+            setTimeout(() => {
+                projectDisplay.classList.remove('transitionOOB');
+            }, 500);
         }       
     });  
 
